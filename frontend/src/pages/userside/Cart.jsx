62a549b57d0a1965bd0e7b5b176a97f46cc43788
Shishir <mac@shihsir.local>
2024-08-28 08:36:47 +0545

    just a basic one

--- frontend/src/pages/userside/Cart.jsx
+++ frontend/src/pages/userside/Cart.jsx
@@ -1,7 +1,40 @@
 import { FaTrashAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+// import KhaltiCheckout from "khalti-web";
 
 const MobileCart = () => {
+//     let config = {
+//   "publicKey": "test_public_key_dc74e0fd57cb46cd93832aee0a507256",
+//   "productIdentity": "1234567890",
+//   "productName": "Drogon",
+//   "productUrl": "http://gameofthrones.com/buy/Dragons",
+//   "eventHandler": {
+//     onSuccess (payload) {
+//       // hit merchant api for initiating verfication
+//       console.log(payload);
+//     },
+//     // onError handler is optional
+//     onError (error) {
+//       // handle errors
+//       console.log(error);
+//     }
+//   },
+//   // one can set the order of payment options and also the payment options based on the order and items in the array
+//   paymentPreference: [
+//     "MOBILE_BANKING",
+//     "KHALTI",
+//     "EBANKING",
+//     "CONNECT_IPS",
+//     "SCT",
+//   ],
+// };
+
+// let checkout = new KhaltiCheckout(config);
+// let btn = document.getElementById("payment-button");
+// btn.onclick = function () {
+// 	checkout.show({amount: 1000});
+// }
+
   return (
     <section className="container mx-auto my-3 flex w-full flex-col gap-3 px-4 ">
       {/* Mobile cart table */}
@@ -46,7 +79,9 @@ const MobileCart = () => {
               </div>
             </div>
           </div>
-          <Link className='px-5 py-3 bg-purple-500 text-white w-40 rounded-xl hover:bg-purple-800'>Pay With Khalti</Link>
+          <Link className='px-5 py-3 bg-purple-500 text-white w-40 rounded-xl hover:bg-purple-800' 
+        //   id='payment-button'
+          >Pay With Khalti</Link>
         
     </section>
   );
